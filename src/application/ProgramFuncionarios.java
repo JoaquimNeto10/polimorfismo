3a32a9e20816f6dcf65b389ddcae00ed4a1b96e9
Joaquim.Neto <engjoaquimnetoo@hotmail.com>
2022-01-10 10:53:11 -0300

    Exercicio terminado

--- src/application/ProgramFuncionarios.java
+++ src/application/ProgramFuncionarios.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import entities.Funcionarios;
 import entities.Terceirizados;
 
-public class Program {
+public class ProgramFuncionarios {
 
 	public static void main(String[] args) {
 		
